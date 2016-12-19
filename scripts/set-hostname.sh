c2291fb0f5258f1c9aeccdeb8a47a3d878428bd3
PastaMasta <pasta.masta2902@gmail.com>
2016-12-19 22:49:29 +0000

    Inital CentOS 7 kickstarts

--- scripts/set-hostname.sh
+++ scripts/set-hostname.sh
@@ -3,13 +3,27 @@
 while true ; do
   printf "\nPlease enter the hostname for this machine: " ; read Hostname
   printf "You have Chosen: ${Hostname} is this correct (y/n)? " ; read reply2
+
   case ${reply2} in
-    [Yy]* ) hostname ${Hostname}
-      echo "NETWORKING=yes" >> /etc/sysconfig/network
-      echo "HOSTNAME=${Hostname}" >> /etc/sysconfig/network
-      echo "DHCP_HOSTNAME=${Hostname}" >> /etc/sysconfig/network-scripts/ifcfg-eth0
-      break ;;
-    [Nn]* ) ;;
-    * ) echo "Please enter (y/n)" ;;
+    [Yy]*)
+
+      if grep -q 'release 6.' /etc/redhat-release ; then
+        hostname ${Hostname}
+        echo "NETWORKING=yes" >> /etc/sysconfig/network
+        echo "HOSTNAME=${Hostname}" >> /etc/sysconfig/network
+        echo "DHCP_HOSTNAME=${Hostname}" >> /etc/sysconfig/network-scripts/ifcfg-eth0
+      elif grep -q 'release 7.' /etc/redhat-release ; then
+        echo "network --hostname=${Hostname}" > /tmp/net-include
+      fi
+      break
+    ;;
+
+    [Nn]* )
+    ;;
+    * )
+      echo "Please enter (y/n)"
+    ;;
+
   esac
 done
+
