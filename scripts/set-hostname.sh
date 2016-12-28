96e45684ddcdae44aa05959f238ae8ffc2859540
PastaMasta <pasta.masta2902@gmail.com>
2016-12-28 18:36:20 +0000

    Fixed hostname selector on el6

--- scripts/set-hostname.sh
+++ scripts/set-hostname.sh
@@ -7,13 +7,13 @@ while true ; do
   case ${reply2} in
     [Yy]*)
 
-      if grep -q 'release 6.' /etc/redhat-release ; then
+      if grep -q 'release 7.' /etc/redhat-release ; then
+        echo "network --hostname=${Hostname}" > /tmp/net-include
+      else
         hostname ${Hostname}
         echo "NETWORKING=yes" >> /etc/sysconfig/network
         echo "HOSTNAME=${Hostname}" >> /etc/sysconfig/network
         echo "DHCP_HOSTNAME=${Hostname}" >> /etc/sysconfig/network-scripts/ifcfg-eth0
-      elif grep -q 'release 7.' /etc/redhat-release ; then
-        echo "network --hostname=${Hostname}" > /tmp/net-include
       fi
       break
     ;;
